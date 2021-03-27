a930e9edc78dbc34c55132d168143b4cfc16b8db
Konrad Milewski <konradmilewski2000@interia.eu>
2021-03-27 21:58:40 +0100

    Dodaj pliki projektów.

---
+++ Pages/Shop.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_DB.Pages
+{
+    public class ShopModel : PageModel
+    {
+        private readonly ILogger<ShopModel> _logger;
+
+        public ShopModel(ILogger<ShopModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
